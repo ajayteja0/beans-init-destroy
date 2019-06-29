f1b431902d9212b396590eb09b791b1c88d774ff
spoors <ajayteja0@gmail.com>
2019-06-29 15:28:17 +0530

    This is Annotation illustration example

---
+++ src/main/java/somespring/annotation/MainApp.java
@@ -0,0 +1,15 @@
+package somespring.annotation;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+   public static void main(String[] args) {
+      @SuppressWarnings("resource")
+	AbstractApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+
+      HelloWorld obj = (HelloWorld) context.getBean("HelloWorld");
+      obj.getMessage();
+      context.registerShutdownHook();
+   }
+}
